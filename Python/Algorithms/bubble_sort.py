f2093caa210b8bbd2d97ad9966d70282d4c20e9f
Salazar <lucassalazar741@gmail.com>
2021-01-17 12:13:14 -0300

    Simplifying code

--- Python/Algorithms/bubble_sort.py
+++ Python/Algorithms/bubble_sort.py
@@ -1,5 +1,4 @@
 from random import randint
-import time
 
 def bubble_sort(arr):
 	for num in range(len(arr) - 1, 0, -1):
@@ -8,16 +7,12 @@ def bubble_sort(arr):
 				temp = arr[i]
 				arr[i] = arr[i + 1]
 				arr[i + 1] = temp
-				
-	return arr
+
 
 arr = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 
 print(f'Unordered list: {arr}')
 
-start = time.process_time()
 bubble_sort(arr)
-end = time.process_time()
 
 print(f'Ordered list: {arr}')
-print(f'Time: {end - start}')
